50dacc2719dc5dd694f291dbf9b8b81196b35680
Ziqi Lin <94877738+Ariosolzq@users.noreply.github.com>
2024-01-20 01:26:32 +0800

    Add files via upload

---
+++ C#-CS architecture/test/frmLogin.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace test
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtUserID = new System.Windows.Forms.TextBox();
+            this.txtPWD = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.cmdCancel = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(171, 221);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "账号：";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("华文彩云", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Location = new System.Drawing.Point(202, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(387, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "足不出户吃遍北京小吃";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("华文彩云", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.ForeColor = System.Drawing.Color.Brown;
+            this.label3.Location = new System.Drawing.Point(162, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(470, 74);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "欢迎使用拼拼";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(171, 270);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "密码：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(272, 409);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(259, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "没有账号？点击这里注册";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtUserID
+            // 
+            this.txtUserID.Location = new System.Drawing.Point(272, 221);
+            this.txtUserID.Name = "txtUserID";
+            this.txtUserID.Size = new System.Drawing.Size(271, 28);
+            this.txtUserID.TabIndex = 5;
+            this.txtUserID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtUserID.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtPWD
+            // 
+            this.txtPWD.Location = new System.Drawing.Point(272, 266);
+            this.txtPWD.Name = "txtPWD";
+            this.txtPWD.Size = new System.Drawing.Size(271, 28);
+            this.txtPWD.TabIndex = 6;
+            this.txtPWD.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtPWD.UseSystemPasswordChar = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnLogin.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnLogin.Location = new System.Drawing.Point(272, 323);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(91, 47);
+            this.btnLogin.TabIndex = 7;
+            this.btnLogin.Text = "登录";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // cmdCancel
+            // 
+            this.cmdCancel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.cmdCancel.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.cmdCancel.Location = new System.Drawing.Point(452, 323);
+            this.cmdCancel.Name = "cmdCancel";
+            this.cmdCancel.Size = new System.Drawing.Size(91, 47);
+            this.cmdCancel.TabIndex = 8;
+            this.cmdCancel.Text = "取消";
+            this.cmdCancel.UseVisualStyleBackColor = false;
+            this.cmdCancel.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 10);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 30;
+            this.dataGridView1.Size = new System.Drawing.Size(519, 150);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(620, 134);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 26);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::test.Properties.Resources._1485441836553;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cmdCancel);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPWD);
+            this.Controls.Add(this.txtUserID);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmLogin";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtUserID;
+        private System.Windows.Forms.TextBox txtPWD;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button cmdCancel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ C#-CS architecture/test/frmLogin.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Common;
+using System.Data.OleDb;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+//using System.Data.OleDb;
+
+
+
+
+namespace test
+{
+    public partial class frmLogin : Form
+    {
+        //数据源
+        /*public const string connString = "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=172.27.152.151)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=ZCL50)));Persist Security Info=True;User ID=shanmu;Password=sm888;";
+        OracleConnection conn = new OracleConnection(connString);//实例化*/
+
+        public static string ConnectOracle()
+        {
+            try
+            {
+                string connString = "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=172.27.152.151)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=ZCL50)));Persist Security Info=True;User ID=shanmu;Password=sm888;";
+                OracleConnection con = new OracleConnection(connString);
+                con.Open(); return "连接成功";
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtUserID.Text = "";
+            txtPWD.Text = "";
+            txtUserID.Focus();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            frmLogin frmlogin = new frmLogin();
+            //MDI frm = new MDI();
+            if (txtUserID.Text == "" || txtPWD.Text == "")
+            {
+                MessageBox.Show("用户账号和密码不能为空！");
+            }
+           /* if (txtstID.Text == "" || txtstName.Text == "")
+            {
+                MessageBox.Show("学号和姓名不能为空", "提示", MessageBoxButtons.OK);
+                return (0);
+            }*/
+            else if (txtUserID.TextLength != 11 )
+            {
+                MessageBox.Show("请输入正确的11位手机号", "提示", MessageBoxButtons.OK);
+            }
+            else
+            {
+                //frmstinfo.Show();
+                //frm.Show();
+                this.Close();
+            }
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            string oradb = "Data Source=(DESCRIPTION="
+             + "(ADDRESS=(PROTOCOL=TCP)(HOST=DESKTOP-94OBPDN)(PORT=1521))"
+             + "(CONNECT_DATA=(SERVICE_NAME=ZCL50)));"
+             + "User Id=shanmu;Password=sm888;";
+
+            try
+            {
+                OracleConnection conn = new OracleConnection(oradb);
+                conn.Open();
+                string strsql;
+                strsql = "SELECT * FROM customer";
+                //OleDbCommand mycmOleDBCommand1 = new OleDbCommand(strSQL1, myConnection);
+                OracleCommand cmd = new OracleCommand(strsql , conn);
+                cmd.CommandType = CommandType.Text;
+
+                //大容量数据，可以进行修改，使用DataSet和DataAdapter
+                DataSet ds = new DataSet();
+                OracleDataAdapter da = new OracleDataAdapter();
+                da.SelectCommand = cmd;
+                da.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+
+                //使用DataReader，读取数据
+                OracleDataReader dr = cmd.ExecuteReader();
+                while (dr.Read()) // C#
+                {
+                    comboBox1.Items.Add(dr["cname"].ToString());
+                }
+
+                dr.Close();
+
+                conn.Dispose(); //Close()也可以。
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
